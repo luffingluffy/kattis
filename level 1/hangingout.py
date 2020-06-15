a069e7dee46afc5e434449c8f6aebcbe988a590c
luffingluffy <66120338+luffingluffy@users.noreply.github.com>
2020-06-15 19:29:05 +0800

    Create hangingout.py

---
+++ level 1/hangingout.py
@@ -0,0 +1,18 @@
+l, x = map(int,input().split())
+n = 0
+d = 0
+
+for i in range(x):
+    z = input().split()
+    # no. of times a group is denied entry
+    if z[0] == "enter":
+        if (n + int(z[1]) > l):
+            d += 1
+        # no. of people entering that wasn't denied
+        else:
+            n += int(z[1])
+    # no. of people leaving
+    else:
+        n -= int(z[1])
+
+print(d)
