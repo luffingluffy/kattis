6dde020bc58ad8a806fbb287601cf00479ce3e4a
luffingluffy <66120338+luffingluffy@users.noreply.github.com>
2020-07-18 01:56:24 +0800

    Create birthdaymatchy.py

---
+++ misc/birthdaymatchy.py
@@ -0,0 +1,14 @@
+import math
+
+def probability(b):
+	
+	if b == 0:
+		return 1
+
+	def birthday(x, y):
+		return 1 - (math.factorial(y) / (math.factorial(y - x) * y ** x))
+
+	return birthday(a - (b - 1) * 2, 366 - b) * probability(b - 1)
+
+a, b = map(int, input().split())
+print(probability(b))
