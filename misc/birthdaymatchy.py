b84cb9170a8526efb629cf370b7ac419f1770be9
luffingluffy <66120338+luffingluffy@users.noreply.github.com>
2020-07-18 03:32:39 +0800

    Update birthdaymatchy.py

--- misc/birthdaymatchy.py
+++ misc/birthdaymatchy.py
@@ -8,7 +8,8 @@ def probability(b):
 	def birthday(x, y):
 		return 1 - (math.factorial(y) / (math.factorial(y - x) * y ** x))
 
-	return birthday(a - (b - 1) * 2, 366 - b) * probability(b - 1)
+	return birthday(a - ((b - 1) * 2), 366 - b) * probability(b - 1)
 
+# a = no. of people in group, b = no. of pairs with matching birthdays 
 a, b = map(int, input().split())
 print(probability(b))
